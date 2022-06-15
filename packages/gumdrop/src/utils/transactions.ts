749b293d528b8c8172b3e72cca916af5038045cd
Lawrence Wu <larry.wu@solana.com>
2022-06-15 09:38:39 -0500

    Use Connection.simulateTransaction instead of hand-rolling

--- packages/gumdrop/src/utils/transactions.ts
+++ packages/gumdrop/src/utils/transactions.ts
@@ -146,7 +146,7 @@ export async function sendSignedTransaction({
     let simulateResult: SimulatedTransactionResponse | null = null;
     try {
       simulateResult = (
-        await simulateTransaction(connection, signedTransaction, 'single')
+        await simulateTransaction(connection, signedTransaction)
       ).value;
     } catch (e) {
       log.error('Simulate Transaction error', e);
@@ -176,27 +176,9 @@ export async function sendSignedTransaction({
 async function simulateTransaction(
   connection: Connection,
   transaction: Transaction,
-  commitment: Commitment,
 ): Promise<RpcResponseAndContext<SimulatedTransactionResponse>> {
-  // @ts-ignore
-  transaction.recentBlockhash = await connection._recentBlockhash(
-    // @ts-ignore
-    connection._disableBlockhashCaching,
-  );
-
-  const signData = transaction.serializeMessage();
-  // @ts-ignore
-  const wireTransaction = transaction._serialize(signData);
-  const encodedTransaction = wireTransaction.toString('base64');
-  const config: any = { encoding: 'base64', commitment };
-  const args = [encodedTransaction, config];
-
-  // @ts-ignore
-  const res = await connection._rpcRequest('simulateTransaction', args);
-  if (res.error) {
-    throw new Error('failed to simulate transaction: ' + res.error.message);
-  }
-  return res.result;
+  const message = transaction.compileMessage();
+  return await connection.simulateTransaction(message);
 }
 
 export async function awaitTransactionSignatureConfirmation(
